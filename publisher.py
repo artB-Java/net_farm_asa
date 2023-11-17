d1a8b1e01ba5e5330a631af696c3cb8c74dd09d0
Victor <victoraugusto@conselt.com.br>
2023-11-17 09:40:24 -0300

    atualizacao publisher

--- publisher.py
+++ publisher.py
@@ -16,17 +16,24 @@ class Publisher(object):
             connection = self.create_connection()
             channel = connection.channel()
 
-            channel.exchange_declare(exchange=self.config['exchange'], exchange_type = 'topic')
-            channel.queue_declare(queue='alunos')
-            channel.queue_bind(exchange=self.config['exchange'], queue='alunos', routing_key=routing_key)
+            # Declaração da troca (exchange)
+            channel.exchange_declare(exchange=self.config['exchange'], exchange_type='topic')
 
-            channel.basic_publish(exchange=self.config['exchange'], 
-                                routing_key=routing_key,                               
+            # Criação das filas
+            filas = ["fazendeiros", "animais", "fazendas", "ordenhas", "pesagens"]
+            for fila in filas:
+                channel.queue_declare(queue=fila)
+                channel.queue_bind(exchange=self.config['exchange'], queue=fila, routing_key=routing_key)
+
+            # Publicação da mensagem
+            channel.basic_publish(exchange=self.config['exchange'],
+                                routing_key=routing_key,
                                 body=message)
         except Exception as e:
-            logger.error(f'Erro na conexão do RabbitMQ ==> {e}')       
+            logger.error(f'Erro na conexão do RabbitMQ ==> {e}')
 
     def create_connection(self):
         param = pika.ConnectionParameters(host=self.config['host'], port=self.config['port'])
         return pika.BlockingConnection(param)
+
 
